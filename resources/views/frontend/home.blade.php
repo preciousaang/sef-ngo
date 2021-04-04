911ecdae663770f4f67774e48b0ce337153af550
Precious Ibeagi <preciousaang@gmail.com>
2021-04-04 22:58:12 +0100

    asset adjustment

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,6 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NGO</title>
+
+    <!-- Core CSS-->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/font-awesome.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/settings.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/owl.theme.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/featherlight.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap-select.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
 </head>
 <body>
     
